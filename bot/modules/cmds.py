dadfe186bc4dd1b17c6a58ef639906f277559d28
rohit3607 <rohitplayer87089@gmail.com>
2025-02-16 12:24:53 +0530

    Update cmds.py

--- bot/modules/cmds.py
+++ bot/modules/cmds.py
@@ -248,3 +248,221 @@ async def add_task(client, message):
     
     ani_task = bot_loop.create_task(get_animes(taskInfo.title, taskInfo.link, True))
     await sendMessage(message, f"<i><b>Task Added Successfully!</b></i>\n\n    • <b>Task Name :</b> {taskInfo.title}\n    • <b>Task Link :</b> {args[1]}")
+
+
+
+@bot.on_message(filters.command('add_fsub') & filters.private & filters.user(Var.ADMINS))
+async def add_forcesub(client: Client, message: Message):
+    pro = await message.reply("<b><i>Processing....</i></b>", quote=True)
+    check = 0
+    channel_ids = await db.get_all_channels()
+    fsubs = message.text.split()[1:]
+
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Close ✖️", callback_data="close")]])
+
+    if not fsubs:
+        await pro.edit("<b>You need to add channel IDs\n<blockquote><u>EXAMPLE</u>:\n/add_fsub [channel_ids] :</b> You can add one or multiple channel IDs at a time.</blockquote>", reply_markup=reply_markup)
+        return
+
+    channel_list = ""
+    for id in fsubs:
+        try:
+            id = int(id)
+        except:
+            channel_list += f"<b><blockquote>Invalid ID: <code>{id}</code></blockquote></b>\n\n"
+            continue
+
+        if id in channel_ids:
+            channel_list += f"<blockquote><b>ID: <code>{id}</code>, already exists..</b></blockquote>\n\n"
+            continue
+
+        id = str(id)
+        if id.startswith('-') and id[1:].isdigit() and len(id) == 14:
+            try:
+                data = await client.get_chat(id)
+                link = data.invite_link
+                cname = data.title
+
+                if not link:
+                    link = await client.export_chat_invite_link(id)
+
+                channel_list += f"<b><blockquote>NAME: <a href={link}>{cname}</a> (ID: <code>{id}</code>)</blockquote></b>\n\n"
+                check += 1
+
+            except:
+                channel_list += f"<b><blockquote>ID: <code>{id}</code>\n<i>Unable to add force-sub, check the channel ID or bot permissions properly..</i></blockquote></b>\n\n"
+
+        else:
+            channel_list += f"<b><blockquote>Invalid ID: <code>{id}</code></blockquote></b>\n\n"
+            continue
+
+    if check == len(fsubs):
+        for id in fsubs:
+            await db.add_channel(int(id))
+        await pro.edit(f'<b>Force-sub channel added ✅</b>\n\n{channel_list}', reply_markup=reply_markup, disable_web_page_preview=True)
+
+    else:
+        await pro.edit(f'<b>❌ Error occurred while adding force-sub channels</b>\n\n{channel_list.strip()}\n\n<b><i>Please try again...</i></b>', reply_markup=reply_markup, disable_web_page_preview=True)
+
+
+@bot.on_message(filters.command('del_fsub') & filters.private & filters.user(OWNER_ID))
+async def delete_all_forcesub(client: Client, message: Message):
+    pro = await message.reply("<b><i>Processing....</i></b>", quote=True)
+    channels = await db.get_all_channels()
+    fsubs = message.text.split()[1:]
+
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Close ✖️", callback_data="close")]])
+
+    if not fsubs:
+        return await pro.edit("<b>⁉️ Please, provide valid IDs or arguments\n<blockquote><u>EXAMPLES</u>:\n/del_fsub [channel_ids] :</b> To delete one or multiple specified IDs\n<code>/del_fsub all</code>: To delete all available force-sub IDs</blockquote>", reply_markup=reply_markup)
+
+    if len(fsubs) == 1 and fsubs[0].lower() == "all":
+        if channels:
+            for id in channels:
+                await db.del_channel(id)
+
+            ids = "\n".join(f"<blockquote><code>{channel}</code> ✅</blockquote>" for channel in channels)
+            return await pro.edit(f"<b>⛔️ All available channel IDs are deleted:\n{ids}</b>", reply_markup=reply_markup)
+        else:
+            return await pro.edit("<b><blockquote>⁉️ No channel IDs available to delete</blockquote></b>", reply_markup=reply_markup)
+
+    if len(channels) >= 1:
+        passed = ''
+        for sub_id in fsubs:
+            try:
+                id = int(sub_id)
+            except:
+                passed += f"<b><blockquote><i>Invalid ID: <code>{sub_id}</code></i></blockquote></b>\n"
+                continue
+            if id in channels:
+                await db.del_channel(id)
+
+                passed += f"<blockquote><code>{id}</code> ✅</blockquote>\n"
+            else:
+                passed += f"<b><blockquote><code>{id}</code> not in force-sub channels</blockquote></b>\n"
+
+        await pro.edit(f"<b>⛔️ Provided channel IDs are deleted:\n\n{passed}</b>", reply_markup=reply_markup)
+
+    else:
+        await pro.edit("<b><blockquote>⁉️ No channel IDs available to delete</blockquote></b>", reply_markup=reply_markup)
+
+
+@bot.on_message(filters.command('fsub_chnl') & filters.private & filters.user(Var.ADMINS))
+async def get_forcesub(client: Client, message: Message):
+    pro = await message.reply("<b><i>Processing....</i></b>", quote=True)
+    channels = await db.get_all_channels()
+    channel_list = "<b><blockquote>❌ No force sub channel found!</b></blockquote>"
+    if channels:
+        channel_list = ""
+        for id in channels:
+            await message.reply_chat_action(ChatAction.TYPING)
+            try:
+                data = await client.get_chat(id)
+                link = data.invite_link
+                cname = data.title
+
+                if not link:
+                    link = await client.export_chat_invite_link(id)
+
+                channel_list += f"<b><blockquote>NAME: <a href={link}>{cname}</a>\n(ID: <code>{id}</code>)</blockquote></b>\n\n"
+
+            except:
+                channel_list += f"<b><blockquote>ID: <code>{id}</code>\n<i>Unable to load other details..</i></blockquote></b>\n\n"
+
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Close ✖️", callback_data="close")]])
+    await message.reply_chat_action(ChatAction.CANCEL)
+    await pro.edit(f"<b>⚡ Force-sub channel list:</b>\n\n{channel_list}", reply_markup=reply_markup, disable_web_page_preview=True)
+
+#=====================================================================================##
+#.........Extra Functions.......#
+#=====================================================================================##
+
+# Auto Delete Setting Commands
+@bot.on_message(filters.command('auto_del') & filters.private & filters.user(Var.ADMINS))
+async def autoDelete_settings(client, message):
+    await message.reply_chat_action(ChatAction.TYPING)
+
+    try:
+            timer = convert_time(await db.get_del_timer())
+            if await db.get_auto_delete():
+                autodel_mode = on_txt
+                mode = 'Dɪsᴀʙʟᴇ Mᴏᴅᴇ ❌'
+            else:
+                autodel_mode = off_txt
+                mode = 'Eɴᴀʙʟᴇ Mᴏᴅᴇ ✅'
+
+            await message.reply_photo(
+                photo = autodel_cmd_pic,
+                caption = AUTODEL_CMD_TXT.format(autodel_mode=autodel_mode, timer=timer),
+                reply_markup = InlineKeyboardMarkup([
+                    [InlineKeyboardButton(mode, callback_data='chng_autodel'), InlineKeyboardButton('◈ Sᴇᴛ Tɪᴍᴇʀ ⏱', callback_data='set_timer')],
+                    [InlineKeyboardButton('🔄 Rᴇғʀᴇsʜ', callback_data='autodel_cmd'), InlineKeyboardButton('Cʟᴏsᴇ ✖️', callback_data='close')]
+                ]),
+                message_effect_id = 5107584321108051014 #👍
+            )
+    except Exception as e:
+            reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Cʟᴏsᴇ ✖️", callback_data = "close")]])
+            await message.reply(f"<b>! Eʀʀᴏʀ Oᴄᴄᴜʀᴇᴅ..\n<blockquote>Rᴇᴀsᴏɴ:</b> {e}</blockquote><b><i>Cᴏɴᴛᴀɴᴄᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ: @rohit_1888</i></b>", reply_markup=reply_markup)
+
+
+#Files related settings command
+@bot.on_message(filters.command('files') & filters.private & filters.user(Var.ADMINS))
+async def files_commands(client: Client, message: Message):
+    await message.reply_chat_action(ChatAction.TYPING)
+
+    try:
+        protect_content = hide_caption = channel_button = off_txt
+        pcd = hcd = cbd = '❌'
+        if await db.get_protect_content():
+            protect_content = on_txt
+            pcd = '✅'
+        if await db.get_hide_caption():
+            hide_caption = on_txt
+            hcd = '✅'
+        if await db.get_channel_button():
+            channel_button = on_txt
+            cbd = '✅'
+        name, link = await db.get_channel_button_link()
+
+        await message.reply_photo(
+            photo = files_cmd_pic,
+            caption = FILES_CMD_TXT.format(
+                protect_content = protect_content,
+                hide_caption = hide_caption,
+                channel_button = channel_button,
+                name = name,
+                link = link
+            ),
+            reply_markup = InlineKeyboardMarkup([
+                [InlineKeyboardButton(f'Pʀᴏᴛᴇᴄᴛ Cᴏɴᴛᴇɴᴛ: {pcd}', callback_data='pc'), InlineKeyboardButton(f'Hɪᴅᴇ Cᴀᴘᴛɪᴏɴ: {hcd}', callback_data='hc')],
+                [InlineKeyboardButton(f'Cʜᴀɴɴᴇʟ Bᴜᴛᴛᴏɴ: {cbd}', callback_data='cb'), InlineKeyboardButton(f'◈ Sᴇᴛ Bᴜᴛᴛᴏɴ ➪', callback_data='setcb')],
+                [InlineKeyboardButton('🔄 Rᴇғʀᴇsʜ', callback_data='files_cmd'), InlineKeyboardButton('Cʟᴏsᴇ ✖️', callback_data='close')]
+            ]),
+            message_effect_id = 5107584321108051014 #👍
+        )
+    except Exception as e:
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Cʟᴏsᴇ ✖️", callback_data = "close")]])
+        await message.reply(f"<b>! Eʀʀᴏʀ Oᴄᴄᴜʀᴇᴅ..\n<blockquote>Rᴇᴀsᴏɴ:</b> {e}</blockquote><b><i>Cᴏɴᴛᴀɴᴄᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ: @rohit_1888</i></b>", reply_markup=reply_markup)
+
+#Request force sub mode commad,,,,,,
+@bot.on_message(filters.command('req_fsub') & filters.private & filters.user(Var.ADMINS))
+async def handle_reqFsub(client: Client, message: Message):
+    await message.reply_chat_action(ChatAction.TYPING)
+    try:
+        on = off = ""
+        if await db.get_request_forcesub():
+            on = "🟢"
+            texting = on_txt
+        else:
+            off = "🔴"
+            texting = off_txt
+
+        button = [
+            [InlineKeyboardButton(f"{on} ON", "chng_req"), InlineKeyboardButton(f"{off} OFF", "chng_req")],
+            [InlineKeyboardButton("⚙️ Mᴏʀᴇ Sᴇᴛᴛɪɴɢs ⚙️", "more_settings")]
+        ]
+        await message.reply(text=RFSUB_CMD_TXT.format(req_mode=texting), reply_markup=InlineKeyboardMarkup(button), message_effect_id=5046509860389126442)
+
+    except Exception as e:
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Cʟᴏsᴇ ✖️", callback_data = "close")]])
+        await message.reply(f"<b>! Eʀʀᴏʀ Oᴄᴄᴜʀᴇᴅ..\n<blockquote>Rᴇᴀsᴏɴ:</b> {e}</blockquote><b><i>Cᴏɴᴛᴀɴᴄᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ: @rohit_1888</i></b>", reply_markup=reply_markup)
