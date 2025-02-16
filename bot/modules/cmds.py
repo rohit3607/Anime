e00942ba89320d97a3481d603d02f9e2f05cd412
rohit3607 <rohitplayer87089@gmail.com>
2025-02-16 15:16:09 +0530

    Update cmds.py

--- bot/modules/cmds.py
+++ bot/modules/cmds.py
@@ -34,6 +34,23 @@ from bot.query import *
 # Create a global dictionary to store chat data
 chat_data_cache = {}
 
+#Chcek user subscription by specifying channel id and user id
+async def is_userJoin(client, user_id, channel_id):
+    #REQFSUB = await db.get_request_forcesub()
+    try:
+        member = await client.get_chat_member(chat_id=channel_id, user_id=user_id)
+        return member.status in {ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER}
+
+    except UserNotParticipant:
+        if await db.get_request_forcesub(): #and await privateChannel(client, channel_id):
+                return await db.reqSent_user_exist(channel_id, user_id)
+
+        return False
+
+    except Exception as e:
+        print(f"!Error on is_userJoin(): {e}")
+        return False
+
 
 @bot.on_message(command('start') & private)
 @new_task
