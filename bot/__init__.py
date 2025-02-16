6095f1f25a3bd5a91f7639759572cf7a516fbefd
rohit3607 <rohitplayer87089@gmail.com>
2025-02-16 15:32:13 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -46,6 +46,7 @@ class Var:
     LOG_CHANNEL = int(getenv("LOG_CHANNEL") or 0)
     FILE_STORE = int(getenv("FILE_STORE"))
     ADMINS = list(map(int, getenv("ADMINS", "1242011540").split()))
+    OWNER_ID = list(map(int, getenv("ADMINS", "1242011540").split()))
     
     SEND_SCHEDULE = getenv("SEND_SCHEDULE", "False").lower() == "true"
     BRAND_UNAME = getenv("BRAND_UNAME", "@username")
